eb6d7f55d6606bbcae4a1a0f1960f69bb90c3edf
Azzakhruf <77542170+Noth3r@users.noreply.github.com>
2021-03-11 21:29:16 +0700

    Add files via upload

---
+++ telegram.js
@@ -0,0 +1,46 @@
+const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const app = express()
+
+const bot = new TelegramBot('ISI TOKEN DISINI', {
+    polling: true
+});
+let chatId = "ISI CHAT ID DISINI"
+
+app.get('/gmail/no', async (req, res) => {
+    await bot.sendMessage(chatId, 'Request No Hp')
+    bot.onText(/\/no (.+)/, async (msg, match) => {
+        const resp = match[1]
+        try {
+            res.json({
+                no: resp
+            })
+            await bot.sendMessage(chatId, 'No Hp Sedang di Proses')
+        } catch (e) {}
+    })
+})
+
+app.get('/gmail/otp/:nohp', async (req, res) => {
+    const nohp = req.params.nohp
+    await bot.sendMessage(chatId, `Request OTP => ${nohp}`)
+    bot.onText(/\/o (.+)/, async (msg, match) => {
+        const resp = match[1]
+        try {
+            res.json({
+                otp: resp
+            })
+            await bot.sendMessage(chatId, 'OTP sedang di proses')
+        } catch (e) {}
+    })
+})
+
+app.get('/gmail/success/:email/:success', async (req, res) => {
+    const email = req.params.email
+    const success = req.params.success
+    await bot.sendMessage(chatId, `${email} => ${success}`)
+    res.json({
+        success: 'Berhasil'
+    })
+})
+
+app.listen(3000)
